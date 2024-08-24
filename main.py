4e681d83084eded0eb936ece514a2656111f2c2a
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-08-24 09:31:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Практическое задание по теме "Переменные":
+Completed_Works = 12
+HoursSpent = 1.5
+CourseName = 'Python'
+Time_for_one_task = (HoursSpent/Completed_Works)
+print ('Курс: ' + str(CourseName) + ', всего задач: ' + str(Completed_Works) + ', затраченно часов: ' +
+       str(HoursSpent) + ', среднее время выполнения ' + str(Time_for_one_task) + ' часа.')
